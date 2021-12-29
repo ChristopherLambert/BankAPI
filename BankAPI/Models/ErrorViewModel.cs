bcc8b645d46b61b6d19961282e46ed18555b5184
Christopher Lambert <christopherwhb@gmail.com>
2021-12-29 21:35:30 +0000

    First Commit

---
+++ BankAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BankAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
