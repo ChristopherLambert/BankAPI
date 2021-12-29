127ecd37376d9b1c5d5f1b926647a838311a3888
Christopher Lambert <christopherwhb@gmail.com>
2021-12-29 22:40:19 +0000

    Adicionado EntityFramework

---
+++ Infra/DataBase/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infra.DataBase
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+          : base(options)
+        {
+            //Database.Migrate();
+            //Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
