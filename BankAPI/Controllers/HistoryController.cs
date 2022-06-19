9824dcbb0bf548be7becb30af63d219979ec78ab
Christopher Lambert <christopherwhb@gmail.com>
2022-06-19 16:49:28 +0100

    Fix Format Date

--- BankAPI/Controllers/HistoryController.cs
+++ BankAPI/Controllers/HistoryController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace BankAPI.Controllers
