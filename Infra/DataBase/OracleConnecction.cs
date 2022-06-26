a14055b8a6dd98ff32e8cfc726e7078977b46888
Christopher Lambert <christopherwhb@gmail.com>
2022-06-26 22:10:16 +0100

    COBRANCA por ENTREGA

--- Infra/DataBase/OracleConnecction.cs
+++ Infra/DataBase/OracleConnecction.cs
@@ -59,26 +59,32 @@ namespace Infra.DataBase
                               "FAT.CGCCPF AS FAT_CGCCPF, REV.CNPJ AS REV_CNPJ, FI.CARTEIRA AS FI_CARTEIRA, " +
                               "FI.AGENCIA_FAVORECIDO AS FI_AGENCIA_FAVORECIDO, FI.NOSSONUMERO AS FI_NOSSONUMERO, " +
                               "FI.CLIENTE AS FI_CLIENTE, FI.DTA_EMISSAO AS FI_DTA_EMISSAO, FI.DTA_VENCIMENTO AS FI_DTA_VENCIMENTO, " +
-                              "FI.VAL_TITULO AS FI_VAL_TITULO, FAT.NOME AS FAT_NOME, FAT.LOGRADOURO_COBRANCA AS FAT_LOGRADOURO_COBRANCA, " +
-                              "FAT.NUMERO_COBRANCA AS FAT_NUMERO_COBRANCA, FAT.CEP_COBRANCA AS FAT_CEP_COBRANCA, FAT.BAIRRO_COBRANCA AS FAT_BAIRRO_COBRANCA, " +
-                              "FAT.MUNICIPIO_COBRANCA AS FAT_MUNICIPIO_COBRANCA, FAT.UF_COBRANCA AS FAT_UF_COBRANCA, PJ.CGC AS PJ_CGC, PF.CPF AS PF_CPF " +
+                              "FI.VAL_TITULO AS FI_VAL_TITULO, FAT.NOME AS FAT_NOME, FAT.LOGRADOURO_ENTREGA AS FAT_LOGRADOURO_ENTREGA, " +
+                              "FAT.NUMERO_ENTREGA AS FAT_NUMERO_ENTREGA, FAT.CEP_ENTREGA FAT_CEP_ENTREGA, FAT.BAIRRO_ENTREGA AS FAT_BAIRRO_ENTREGA, " +
+                              "FAT.MUNICIPIO_ENTREGA AS FAT_MUNICIPIO_ENTREGA, FAT.UF_ENTREGA AS FAT_UF_ENTREGA, PJ.CGC AS PJ_CGC, PF.CPF AS PF_CPF " +
                                   "FROM FIN_TITULO FI " +
                                   "INNER JOIN FAT_CLIENTE FAT ON FAT.CLIENTE = FI.CLIENTE " +
                                   "INNER JOIN GER_REVENDA REV ON REV.REVENDA = FI.REVENDA " +
                                   "LEFT JOIN FAT_PESSOA_FISICA PF ON PF.CLIENTE = FI.CLIENTE " +
                                   "LEFT JOIN FAT_PESSOA_JURIDICA PJ ON PJ.CLIENTE = FI.CLIENTE " +
-                                  "WHERE FI.EMPRESA = 1 AND FI.REVENDA = 1 AND BANCO = 104 " +
+
+                                  //FILTRO TESTE
+                                  "WHERE FI.EMPRESA = '1'AND FI.DEPARTAMENTO = '1'" +
+                                  "AND FI.STATUS = 'EM' AND FI.ORIGEM = 207" +
+
+                                  //"WHERE FI.EMPRESA = 1 AND FI.REVENDA = 1 AND BANCO = 104 " +
                                   "AND ROWNUM = 1";
                         }
                         else
-                        {
+                        {   
+                            // Todos os campos vindo de FAT_ENTREGA referente a Endereço
                             cmd.CommandText = "SELECT " +
                                 "FAT.CGCCPF AS FAT_CGCCPF, REV.CNPJ AS REV_CNPJ, FI.CARTEIRA AS FI_CARTEIRA, " +
                                 "FI.AGENCIA_FAVORECIDO AS FI_AGENCIA_FAVORECIDO, FI.NOSSONUMERO AS FI_NOSSONUMERO, " +
                                 "FI.CLIENTE AS FI_CLIENTE, FI.DTA_EMISSAO AS FI_DTA_EMISSAO, FI.DTA_VENCIMENTO AS FI_DTA_VENCIMENTO, " +
-                                "FI.VAL_TITULO AS FI_VAL_TITULO, FAT.NOME AS FAT_NOME, FAT.LOGRADOURO_COBRANCA AS FAT_LOGRADOURO_COBRANCA, " +
-                                "FAT.NUMERO_COBRANCA AS FAT_NUMERO_COBRANCA, FAT.CEP_COBRANCA AS FAT_CEP_COBRANCA, FAT.BAIRRO_COBRANCA AS FAT_BAIRRO_COBRANCA, " +
-                                "FAT.MUNICIPIO_COBRANCA AS FAT_MUNICIPIO_COBRANCA, FAT.UF_COBRANCA AS FAT_UF_COBRANCA, PJ.CGC AS PJ_CGC, PF.CPF AS PF_CPF " +
+                                "FI.VAL_TITULO AS FI_VAL_TITULO, FAT.NOME AS FAT_NOME, FAT.LOGRADOURO_ENTREGA AS FAT_LOGRADOURO_ENTREGA, " +
+                                "FAT.NUMERO_ENTREGA AS FAT_NUMERO_ENTREGA, FAT.CEP_ENTREGA FAT_CEP_ENTREGA, FAT.BAIRRO_ENTREGA AS FAT_BAIRRO_ENTREGA, " +
+                                "FAT.MUNICIPIO_ENTREGA AS FAT_MUNICIPIO_ENTREGA, FAT.UF_ENTREGA AS FAT_UF_ENTREGA, PJ.CGC AS PJ_CGC, PF.CPF AS PF_CPF " +
                                     "FROM FIN_TITULO FI " +
                                     "INNER JOIN FAT_CLIENTE FAT ON FAT.CLIENTE = FI.CLIENTE " +
                                     "INNER JOIN GER_REVENDA REV ON REV.REVENDA = FI.REVENDA " +
@@ -108,8 +114,9 @@ namespace Infra.DataBase
                             try
                             {
                                 var titulo = new BradescoBoleto();
-                                titulo.nuCPFCNPJ = Convert.ToInt64(dataReader["FAT_CGCCPF"].ToString().Substring(0,9));
-                                titulo.filialCPFCNPJ = Convert.ToInt64(dataReader["FAT_CGCCPF"].ToString().Substring(9, 3)); // **
+                                titulo.nuCPFCNPJ = Convert.ToInt64(dataReader["FAT_CGCCPF"].ToString().Substring(0,9));            
+                                // titulo.filialCPFCNPJ = Convert.ToInt64(dataReader["FAT_CGCCPF"].ToString().Substring(9, 3)); // **
+                                titulo.filialCPFCNPJ = Convert.ToInt64(dataReader["FAT_CGCCPF"].ToString().Substring(7, 3));
                                 titulo.ctrlCPFCNPJ = Convert.ToInt64(dataReader["REV_CNPJ"].ToString().Substring(13)); // 2 ultimos digitos
                                 titulo.idProduto = Convert.ToInt64(dataReader["FI_CARTEIRA"] != DBNull.Value ? dataReader["FI_CARTEIRA"] : 0);
                                 titulo.nuNegociacao = Convert.ToInt64(dataReader["FI_AGENCIA_FAVORECIDO"] != DBNull.Value ? dataReader["FI_AGENCIA_FAVORECIDO"] : 0);
@@ -120,14 +127,19 @@ namespace Infra.DataBase
                                 titulo.vlNominalTitulo = Convert.ToDecimal(dataReader["FI_VAL_TITULO"] != DBNull.Value ? dataReader["FI_VAL_TITULO"] : 0);
                                 titulo.cdEspecieTitulo = 32; // BOLETO --> (BDP -- Boleto de Proposta) Docu bradesco)
                                 titulo.nomePagador = dataReader["FAT_NOME"].ToString();
-                                titulo.logradouroPagador = dataReader["FAT_LOGRADOURO_COBRANCA"].ToString().Length > 0 ? dataReader["FAT_LOGRADOURO_COBRANCA"].ToString() : "0";
-                                titulo.nuLogradouroPagador = dataReader["FAT_NUMERO_COBRANCA"].ToString().Length > 0 ? dataReader["FAT_NUMERO_COBRANCA"].ToString() : "0";
-                                titulo.cepPagador = Convert.ToInt64(dataReader["FAT_CEP_COBRANCA"] != DBNull.Value ? dataReader["FAT_CEP_COBRANCA"] : 0);
+                                titulo.logradouroPagador = dataReader["FAT_LOGRADOURO_ENTREGA"].ToString().Length > 0 ? dataReader["FAT_LOGRADOURO_ENTREGA"].ToString() : "0";
+                                titulo.nuLogradouroPagador = dataReader["FAT_NUMERO_ENTREGA"].ToString().Length > 0 ? dataReader["FAT_NUMERO_ENTREGA"].ToString() : "0";
+
+                                // titulo.cepPagador = 80740; // PEGAR 5 PRIMEIROS DISGITOS DO CEP 
+                                var cep = dataReader["FAT_CEP_ENTREGA"] != DBNull.Value ? dataReader["FAT_CEP_ENTREGA"] : 0;
+                                titulo.cepPagador = Convert.ToInt64(cep.ToString().Substring(0,5));
+                          
                                 titulo.complementoCepPagador = 0;
-                                titulo.bairroPagador = dataReader["FAT_BAIRRO_COBRANCA"].ToString().Length > 0 ? dataReader["FAT_BAIRRO_COBRANCA"].ToString() : "0";
-                                titulo.municipioPagador = dataReader["FAT_MUNICIPIO_COBRANCA"].ToString().Length > 0 ? dataReader["FAT_MUNICIPIO_COBRANCA"].ToString() : "0";
-                                titulo.ufPagador = dataReader["FAT_UF_COBRANCA"].ToString().Length > 0 ? dataReader["FAT_UF_COBRANCA"].ToString() : "0";
-                                titulo.nuCpfcnpjPagador = Convert.ToInt64(dataReader["PJ_CGC"] ?? dataReader["PF_CPF"]);
+                                titulo.bairroPagador = dataReader["FAT_BAIRRO_ENTREGA"].ToString().Length > 0 ? dataReader["FAT_BAIRRO_ENTREGA"].ToString() : "0";
+                                titulo.municipioPagador = dataReader["FAT_MUNICIPIO_ENTREGA"].ToString().Length > 0 ? dataReader["FAT_MUNICIPIO_ENTREGA"].ToString() : "0";
+                                titulo.ufPagador = dataReader["FAT_UF_ENTREGA"].ToString().Length > 0 ? dataReader["FAT_UF_ENTREGA"].ToString() : "0";
+                                // titulo.nuCpfcnpjPagador = Convert.ToInt64(dataReader["PJ_CGC"] ?? dataReader["PF_CPF"]);
+                                titulo.nuCpfcnpjPagador = Convert.ToInt64(dataReader["PJ_CGC"] != DBNull.Value ? dataReader["PJ_CGC"] : dataReader["PF_CPF"]);
                                 titulo.cdIndCpfcnpjPagador = titulo.nuCpfcnpjPagador.ToString().Length == 14 ? 2 : 1;
                                 listTitulos.Add(titulo);
                             }
